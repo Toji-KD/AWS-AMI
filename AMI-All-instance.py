d09c034e54cf5b6d2c1f34ceaa75d413c4dd2ded
root <root@ip-172-31-8-114.ec2.internal>
2017-07-06 21:02:15 +0000

    New hashy

--- AMI-All-instance.py
+++ AMI-All-instance.py
@@ -9,7 +9,7 @@ Retention = 0                #
 
 #This scipt takes AMI of all instances in the region configured in AWS-CLI
 #AMIs and its associated snapshots will be removed according to the retention period.
-#
+##
 ##### Server Requirements ####################
 # AWS CLI - # pip install awscli , # aws configure
 # Python3 -   Default in all servers.
