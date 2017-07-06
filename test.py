14532f2669f3261e58f488ae9f4f06877f905618
Toji K Dominic <tojikdominic@gmail.com>
2017-07-07 03:54:58 +0530

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-import datetime
-import boto3
-Time_Now = datetime.datetime.now().strftime("%Y/%m/%d-%H/%M/%S")
-ec2 = boto3.resource('ec2')
-client = boto3.client('ec2')
-for instance in ec2.instances.all():
-        response = client.create_image(Description='AMI of '+instance.id+' Dated '+Time_Now, InstanceId=instance.id, Name='AMI of '+instance.id+' Dated'+Time_Now, NoReboot=True)
